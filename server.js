f81c29e125ead1d16b956be29b46c3c762dce605
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-08-09 15:01:19 -0500

    added id filtering

--- server.js
+++ server.js
@@ -4,9 +4,24 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const { notes } = require('./Develop/db/notes.json');
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(notes => notes.id === id)[0];
+    return result;
+  }
+  
+
 app.get('/api/notes', (req, res) => {
     res.json(notes);
     console.log(notes)
+  });
+  app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if(result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+      
   });
   
   app.post('/api/notes', (req, res) => {
