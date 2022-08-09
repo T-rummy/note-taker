5629581085be80d278d9c3023e0c6c31f526e377
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-08-09 15:23:44 -0500

    posting new notes to json

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const { notes } = require('./Develop/db/notes.json');
 
 function findById(id, notesArray) {
     const result = notesArray.filter(notes => notes.id === id)[0];
     return result;
   }
+
+  function createNewNote(body, notesArray) {
+
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/notes.json'),
+        JSON.stringify({notes: notesArray}, null, 2)
+    );
+    return note;
+  }
+  
   
 
 app.get('/api/notes', (req, res) => {
@@ -25,9 +41,15 @@ app.get('/api/notes', (req, res) => {
   });
   
   app.post('/api/notes', (req, res) => {
-    res.json(notes);
-    console.log(notes)
+
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+
+    console.log(req.body);
+    res.json(req.body);
   });
+  
  
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
